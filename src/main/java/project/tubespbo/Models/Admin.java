5a46a8470b8dd04e3e1679a2fd9402f4e7812b41
spirinity <11231037@student.itk.ac.id>
2024-05-11 00:35:16 +0800

    added admin and nasabah class

---
+++ src/main/java/project/tubespbo/Models/Admin.java
@@ -0,0 +1,32 @@
+package project.tubespbo.Models;
+
+public class Admin {
+    private String nama;
+    private String username;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+}
