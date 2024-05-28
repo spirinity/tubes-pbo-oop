09281eb8b33bce57da5121892579b5842629daa7
arka <arkamahardika35@gmail.com>
2024-05-28 23:53:45 +0800

    feat : adding add admin controller

--- src/main/java/project/tubespbo/Controller/Admin/AddAdminController.java
+++ src/main/java/project/tubespbo/Controller/Admin/AddAdminController.java
@@ -13,38 +13,48 @@ import java.sql.SQLException;
 public class AddAdminController {
 
     @FXML
-    private TextField namaField;
+    private TextField usernameField;
 
     @FXML
-    private TextField hargaField;
+    private TextField passwordField;
+
+    @FXML
+    private TextField namaLengkapField;
+
+    @FXML
+    private TextField alamatField;
 
     @FXML
     private Label messageLabel;
 
     @FXML
     public void tambahOnAction() {
-        String nama = namaField.getText();
-        int harga;
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        String namaLengkap = namaLengkapField.getText();
+        String alamat =  alamatField.getText();
 
         try {
-            harga = Integer.parseInt(hargaField.getText());
+             password = (passwordField.getText());
         } catch (NumberFormatException e) {
             messageLabel.setText("Mohon untuk menlengkapi isi data!");
             return;
         }
 
-        String query = "INSERT INTO sampah (nama_sampah, harga_sampah) VALUES (?, ?)";
+        String query = "INSERT INTO users (username, password, role, nama_lengkap, alamat) VALUES (?, ?, 'admin', ?, ?)";
         DatabaseConnection dbConnection = new DatabaseConnection(); // Create an instance
         try (Connection conn = dbConnection.getConnection(); // Call the method on the instance
              PreparedStatement pstmt = conn.prepareStatement(query)) {
-            pstmt.setString(1, nama);
-            pstmt.setDouble(2, harga);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setString(3, namaLengkap);
+            pstmt.setString(4, alamat);
             pstmt.executeUpdate();
-            messageLabel.setText("Sampah berhasil ditambah!");
+            messageLabel.setText("Admin berhasil ditambah!");
             closeWindow();
         } catch (SQLException e) {
             e.printStackTrace();
-            messageLabel.setText("Gagal untuk menambahkan sampah!");
+            messageLabel.setText("Gagal untuk menambahkan Admin!");
         }
     }
 
@@ -54,7 +64,7 @@ public class AddAdminController {
     }
 
     private void closeWindow() {
-        Stage stage = (Stage) namaField.getScene().getWindow();
+        Stage stage = (Stage) usernameField.getScene().getWindow();
         stage.close();
     }
 }
