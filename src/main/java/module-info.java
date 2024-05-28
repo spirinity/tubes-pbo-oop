b6aec49689873b8c15d05ce8b34cea635ad26d0c
arka <arkamahardika35@gmail.com>
2024-05-28 17:29:17 +0800

    fixing the module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
-module com.example.tubespbo {
+module project.tubespbo {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
+    requires mysql.connector.j;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -11,6 +13,14 @@ module com.example.tubespbo {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    opens Controller to javafx.fxml;
-    exports Controller;
+    opens project.tubespbo.Controller to javafx.fxml;
+    exports project.tubespbo.Controller;
+    exports project.tubespbo.Util;
+    opens project.tubespbo.Util to javafx.fxml;
+    exports project.tubespbo.Controller.Admin;
+    opens project.tubespbo.Controller.Admin to javafx.fxml;
+    opens project.tubespbo.Models to javafx.base;
+    exports project.tubespbo.Models;
+    exports project.tubespbo.Controller.Nasabah;
+    opens project.tubespbo.Controller.Nasabah to javafx.fxml;
 }
