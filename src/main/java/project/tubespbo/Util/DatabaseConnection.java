e91a6350aaa777e4078bad129458f9f8af5ffe3a
arka <arkamahardika35@gmail.com>
2024-05-29 22:37:40 +0800

    refactoring and added util folder

--- src/main/java/project/tubespbo/Util/DatabaseConnection.java
+++ src/main/java/project/tubespbo/Util/DatabaseConnection.java
@@ -8,17 +8,17 @@ public class DatabaseConnection {
 
     public Connection getConnection() {
         String databaseName = "bank_sampah";
-        String databaseUser = "root"; // Update with your database
-        String databasePassword = ""; // Update with your database password
+        String databaseUser = "root";
+        String databasePassword = "";
         String url = "jdbc:mysql://localhost/" + databaseName;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-            System.out.println("Database connected successfully"); // Print success message
+            System.out.println("Database connected successfully");
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Failed to connect to the database"); // Print error message
+            System.out.println("Failed to connect to the database");
         }
 
         return databaseLink;
