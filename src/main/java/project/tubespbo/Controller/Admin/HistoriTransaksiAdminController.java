4221015bb5216c7e9f9311fe1b1bc96486623e2b
arka <arkamahardika35@gmail.com>
2024-05-30 23:57:20 +0800

    feat : adding histori transaksi admin

---
+++ src/main/java/project/tubespbo/Controller/Admin/HistoriTransaksiAdminController.java
@@ -0,0 +1,206 @@
+package project.tubespbo.Controller.Admin;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import project.tubespbo.Models.*;
+import project.tubespbo.Util.DatabaseConnection;
+import project.tubespbo.Util.Session;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class HistoriTransaksiAdminController {
+
+    private Stage stage;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private Button managementSampahButton;
+
+    @FXML
+    private Button managementUserButton;
+
+    @FXML
+    private Button transaksiButton;
+    @FXML
+    private Button historiTransaksiButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TableView<Transaksi> historiTransaksiTableView;
+
+    @FXML
+    private TableColumn<Transaksi, String> usernameColumn;
+
+    @FXML
+    private TableColumn<Transaksi, String> sampahColumn;
+
+    @FXML
+    private TableColumn<Transaksi, Integer> beratColumn;
+
+    @FXML
+    private TableColumn<Transaksi, Integer> hargaColumn;
+
+    @FXML
+    private TableColumn<Transaksi, String> statusColumn;
+
+    @FXML
+    private TableColumn<Transaksi, Date> tanggalColumn;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+        stage.setResizable(false);
+        stage.setWidth(600);
+        stage.setHeight(450);
+    }
+
+    @FXML
+    private void dashboardOnAction(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/Admin/DashboardAdminView.fxml"));
+        Parent root = loader.load();
+        DashboardAdminController dashboardAdminController = loader.getController();
+        dashboardAdminController.setStage((Stage) dashboardButton.getScene().getWindow()); // Set the stage instance
+        // Get the current scene and set its root to the new scene's root
+        Scene currentScene = dashboardButton.getScene();
+        currentScene.setRoot(root);
+    }
+
+    @FXML
+    private void managementSampahOnAction(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/Admin/ManagementSampahView.fxml"));
+        Parent root = loader.load();
+        ManagementSampahController managementSampahController = loader.getController();
+        managementSampahController.setStage((Stage) managementSampahButton.getScene().getWindow()); // Set the stage instance
+        // Get the current scene and set its root to the new scene's root
+        Scene currentScene = managementSampahButton.getScene();
+        currentScene.setRoot(root);
+    }
+
+    @FXML
+    private void managementUserOnAction(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/Admin/ManagementUserAdminView.fxml"));
+        Parent root = loader.load();
+        ManagementUserController managementUserController = loader.getController();
+        managementUserController.setStage((Stage) managementUserButton.getScene().getWindow()); // Set the stage instance
+        // Get the current scene and set its root to the new scene's root
+        Scene currentScene = managementUserButton.getScene();
+        currentScene.setRoot(root);
+    }
+
+    @FXML
+    private void transaksiOnAction(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/Admin/TransaksiAdminView.fxml"));
+        Parent root = loader.load();
+        TransaksiAdminController transaksiAdminController = loader.getController();
+        transaksiAdminController.setStage((Stage) transaksiButton.getScene().getWindow()); // Set the stage instance
+        // Get the current scene and set its root to the new scene's root
+        Scene currentScene = transaksiButton.getScene();
+        currentScene.setRoot(root);
+    }
+
+    @FXML
+    private void historiTransaksiOnAction(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/Admin/HistoriTransaksiAdminView.fxml"));
+        Parent root = loader.load();
+        HistoriTransaksiAdminController historiTransaksiAdminController = loader.getController();
+        historiTransaksiAdminController.setStage((Stage) historiTransaksiButton.getScene().getWindow()); // Set the stage instance
+        // Get the current scene and set its root to the new scene's root
+        Scene currentScene = historiTransaksiButton.getScene();
+        currentScene.setRoot(root);
+    }
+
+    @FXML
+    private void logoutOnAction(ActionEvent e) throws IOException {
+        Session.getInstance().clearSession();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/tubespbo/Views/LoginView.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) logoutButton.getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    public void initialize() {
+        usernameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getUser().getUsername()));
+        sampahColumn.setCellValueFactory(new PropertyValueFactory<>("sampah"));
+        beratColumn.setCellValueFactory(new PropertyValueFactory<>("berat"));
+        hargaColumn.setCellValueFactory(new PropertyValueFactory<>("harga"));
+        tanggalColumn.setCellValueFactory(new PropertyValueFactory<>("tanggal"));
+
+        Entity currentUser = Session.getInstance().getCurrentUser();
+
+        // Display the username in the label
+        usernameLabel.setText(currentUser.getUsername() + " (Admin)");
+        loadHistoriTransaksi();
+    }
+
+    private void loadHistoriTransaksi() {
+        ObservableList<Transaksi> transaksiList = FXCollections.observableArrayList();
+        String query = "SELECT transaksi.id, transaksi.berat, transaksi.harga, transaksi.status, transaksi.tanggal, users.username, users.role, users.nama_lengkap, users.alamat, sampah.nama_sampah as nama, sampah.harga_sampah as harga " +
+                "FROM transaksi " +
+                "JOIN users ON transaksi.user_id = users.id " +
+                "JOIN sampah ON transaksi.sampah_id = sampah.id " + "WHERE status = 'Terkonfirmasi'";
+
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int berat = rs.getInt("berat");
+                int harga = rs.getInt("harga");
+                String status = rs.getString("status");
+                Date tanggal = rs.getDate("tanggal");
+                String username = rs.getString("username");
+                String role = rs.getString("role");
+                String namaLengkap = rs.getString("nama_lengkap");
+                String alamat = rs.getString("alamat");
+
+                int sampahId = rs.getInt("id");
+                String sampahNama = rs.getString("nama");
+                int sampahHarga = rs.getInt("harga");
+                Sampah sampah = new Sampah(sampahId, sampahNama, sampahHarga);
+
+                Entity user;
+                if ("admin".equalsIgnoreCase(role)) {
+                    user = new Admin(null, username, null, namaLengkap, alamat);
+                } else if ("nasabah".equalsIgnoreCase(role)) {
+                    user = new Nasabah(null, username, null, namaLengkap, alamat);
+                } else {
+                    continue; // Skip if role is neither 'admin' nor 'nasabah'
+                }
+
+                Transaksi transaksi = new Transaksi(id, user, sampah, berat, harga, status, tanggal);
+                transaksiList.add(transaksi);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        historiTransaksiTableView.setItems(transaksiList);
+    }
+}
